d7a45938f52687b8513762aba0ebb2e076095b79
David Durst <davidbdurst@gmail.com>
2019-02-12 19:09:09 -0800

    added html generator with example files

--- html_gen/make_html.sh
+++ html_gen/make_html.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-HALIDE_PROGRAM_RESULTS_FILE=$1
+HALIDE_CPU_PROGRAM_RESULTS_FILE=$1
+HALIDE_COREIR_PROGRAM_RESULTS_FILE=$2
+HALIDE_TEMP_RESULTS="temp_results.log"
 export INPUT_HTML_FILE_PRELUDE="index_prelude.html"
 export INPUT_HTML_FILE_EPILOGUE="index_epilogue.html"
 export OUTPUT_HTML_FILE="index.html"
 
 cp $INPUT_HTML_FILE_PRELUDE $OUTPUT_HTML_FILE
-sed -e "s/\^\[\[0;3[0-9]m\(\w*D\).*/\1/" $HALIDE_PROGRAM_RESULTS_FILE >> $OUTPUT_HTML_FILE
-sed -rEi 's/([a-zA-Z\-]+\=[0-9]+)[[:space:]]+(PASSED)/<tr class="table-success"><td>\1<\/td><td>\2<\/td><\/tr>/' $OUTPUT_HTML_FILE 
-sed -rEi 's/([a-zA-Z\-]+\=[0-9]+)[[:space:]]+(FAILED)/<tr class="table-danger"><td>\1<\/td><td>\2<\/td><\/tr>/' $OUTPUT_HTML_FILE 
+# zip the two inputs together
+paste -d " " $HALIDE_CPU_PROGRAM_RESULTS_FILE $HALIDE_COREIR_PROGRAM_RESULTS_FILE > $HALIDE_TEMP_RESULTS
+# remove the color formatting for the terminal
+sed -rEi 's/\^\[\[0?;?[0-9]+m//g' $HALIDE_TEMP_RESULTS
+## put every entry in the file in a html row
+sed -rEi 's/(.*)/<tr>\1<\/tr>/' $HALIDE_TEMP_RESULTS
+sed -rEi 's/([a-zA-Z\-]+\=[0-9]+)[[:space:]]+(PASSED)/<td class="table-success">\1<\/td><td class="table-success">\2<\/td>/g' $HALIDE_TEMP_RESULTS 
+sed -rEi 's/([a-zA-Z\-]+\=[0-9]+)[[:space:]]+(FAILED)/<td class="table-danger">\1<\/td><td class="table-danger">\2<\/td>/g' $HALIDE_TEMP_RESULTS 
+cat $HALIDE_TEMP_RESULTS >> $OUTPUT_HTML_FILE
 echo "" >> $OUTPUT_HTML_FILE
 cat $INPUT_HTML_FILE_EPILOGUE >> $OUTPUT_HTML_FILE
-
+rm $HALIDE_TEMP_RESULTS
