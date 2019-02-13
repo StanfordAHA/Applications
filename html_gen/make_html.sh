46150400c26fb9bd8afbaf3a297b28d12503cc93
David Durst <davidbdurst@gmail.com>
2019-02-12 19:58:37 -0800

    make_html using linux flags now

--- html_gen/make_html.sh
+++ html_gen/make_html.sh
@@ -6,16 +6,16 @@ export INPUT_HTML_FILE_PRELUDE="html_gen/index_prelude.html"
 export INPUT_HTML_FILE_EPILOGUE="html_gen/index_epilogue.html"
 export OUTPUT_HTML_FILE="results/index.html"
 
-mkdir results
+mkdir -p results
 cp $INPUT_HTML_FILE_PRELUDE $OUTPUT_HTML_FILE
 # zip the two inputs together
 paste -d " " $HALIDE_CPU_PROGRAM_RESULTS_FILE $HALIDE_COREIR_PROGRAM_RESULTS_FILE > $HALIDE_TEMP_RESULTS
 # remove the color formatting for the terminal
-sed -rEi 's/\^\[\[0?;?[0-9]+m//g' $HALIDE_TEMP_RESULTS
+sed -ri -e 's/\^\[\[0?;?[0-9]+m//g' $HALIDE_TEMP_RESULTS
 ## put every entry in the file in a html row
-sed -rEi 's/(.*)/<tr>\1<\/tr>/' $HALIDE_TEMP_RESULTS
-sed -rEi 's/([a-zA-Z\-]+\=[0-9]+)[[:space:]]+(PASSED)/<td class="table-success">\1<\/td><td class="table-success">\2<\/td>/g' $HALIDE_TEMP_RESULTS 
-sed -rEi 's/([a-zA-Z\-]+\=[0-9]+)[[:space:]]+(FAILED)/<td class="table-danger">\1<\/td><td class="table-danger">\2<\/td>/g' $HALIDE_TEMP_RESULTS 
+sed -ri -e 's/(.*)/<tr>\1<\/tr>/' $HALIDE_TEMP_RESULTS
+sed -ri -e 's/([a-zA-Z\-]+\=[0-9]+)[[:space:]]+(PASSED)/<td class="table-success">\1<\/td><td class="table-success">\2<\/td>/g' $HALIDE_TEMP_RESULTS 
+sed -ri -e 's/([a-zA-Z\-]+\=[0-9]+)[[:space:]]+(FAILED)/<td class="table-danger">\1<\/td><td class="table-danger">\2<\/td>/g' $HALIDE_TEMP_RESULTS 
 cat $HALIDE_TEMP_RESULTS >> $OUTPUT_HTML_FILE
 echo "" >> $OUTPUT_HTML_FILE
 cat $INPUT_HTML_FILE_EPILOGUE >> $OUTPUT_HTML_FILE
