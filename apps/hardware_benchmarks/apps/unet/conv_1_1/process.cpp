6eb71939875d61ba8dd0b131a3981826fa84f3a3
Kathleen Feng <kzf@stanford.edu>
2019-05-22 15:09:46 -0700

    update unet layers with new hardware benchmark tester

--- apps/hardware_benchmarks/apps/unet/conv_1_1/process.cpp
+++ apps/hardware_benchmarks/apps/unet/conv_1_1/process.cpp
@@ -17,20 +17,22 @@ int main(int argc, char **argv) {
   int z = 4;
   int w = 1;
 
-  std::vector<Buffer<uint8_t>> inputs;
-  std::vector<Buffer<uint8_t>> outputs;
-
-  inputs.push_back(Buffer<uint8_t>(x, y, z));
-  inputs.push_back(Buffer<uint8_t>(k_x, k_y, z, w)); // kernel
-  outputs.push_back(Buffer<uint8_t>(x - k_x + 1, y - k_y + 1, w));
+  std::vector<Buffer<uint8_t>*> inputs;
+  std::vector<Buffer<uint8_t>*> outputs;
+  Buffer<uint8_t> image(x, y, z);
+  Buffer<uint8_t> kernel(k_x, k_y, z, w);
+  Buffer<uint8_t> output(x - k_x + 1, y - k_y + 1, w);
+  inputs.push_back(&image);
+  inputs.push_back(&kernel);
+  outputs.push_back(&output);
 
   General_ProcessController<uint8_t> processor("conv",
                                             {
                                               {"cpu",
-                                                  [&]() { conv(inputs[0], inputs[1], outputs[0]); }
+                                                  [&]() { conv(*inputs[0], *inputs[1], *outputs[0]); }
                                               },
                                               {"coreir",
-                                                  [&]() { run_coreir_on_interpreter<>("bin/design_top.json", inputs[0], outputs[0],
+                                                  [&]() { run_coreir_on_interpreter<>("bin/design_top.json", *inputs[0], *outputs[0],
                                                                                       "self.in_arg_0_0_0", "self.out_0_0"); }
                                               }
 
